99ab9d0a55e0739195488a762316ee7899beec7c
songhao <songhao@songhaodeMacBook-Pro.local>
2017-05-28 21:43:30 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
     if @group.save
       redirect_to groups_path
       flash[:notice] = "新建成功"
