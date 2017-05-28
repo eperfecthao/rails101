9f730abcf7c75294d801d38e3f6c22f78362e58a
songhao <songhao@songhaodeMacBook-Pro.local>
2017-05-28 20:59:06 +0800

    implement CRUD

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,55 @@
+class GroupsController < ApplicationController
+
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+      flash[:notice] = "新建成功"
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path
+      flash[:notice] = "修改成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to groups_path
+    flash[:warning] = "删除成功"
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+end
