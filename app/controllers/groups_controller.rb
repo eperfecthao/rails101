54931e0c940bee3650e50f253484229ff6ba1b30
songhao <songhao@songhaodeMacBook-Pro.local>
2017-05-28 21:34:07 +0800

    implement log in/out

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
 
   before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @groups = Group.all
